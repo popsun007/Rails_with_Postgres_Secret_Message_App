85ee7d516e73d5e790cc485e1b164a252f4fa6ce
Frank Sun <popsun2010@gmail.com>
2015-10-18 15:16:16 -0700

    Add log in feature and test

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def logging_in
+  end
+
+  def checking_in
+    user = User.find_by_email(params[:email])
+    if user != nil && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      redirect_to "/users/#{user.id}"
+    else
+      flash[:notice] = "Invalid Username or Password"
+      redirect_to "/sessions/new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :name, presence: true
+  validates :email, presence: true, :uniqueness => { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  has_secure_password
+end
