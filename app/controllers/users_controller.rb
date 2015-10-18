94ce5f12a3fd1aed6700a354b6679f7cee6606ec
Frank Sun <popsun2010@gmail.com>
2015-10-18 16:57:54 -0700

    Add update user profile feature and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to "/users/#{user.id}"
+  end
+
   def logging_in
   end
 
